3fbd2225e3776d297f67250cd28a97f07a228ae6
yery <yeryrodriguez967@gmail.com>
2026-07-11 09:22:40 -0500

    proyecto-reach-main

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Tarjeta from './components/Tarjeta';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Fcf from './assets/lol.webp';
+import Fcf from './assets/logo.jpg';
 import Inicio from './pages/inicio';
 import Servicios from './pages/servicios';
 
